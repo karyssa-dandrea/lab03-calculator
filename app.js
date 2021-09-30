14116c6f9f9da99b7e31a15d8b7c366582678002
karyssa-dandrea <karyssadandrea@gmail.com>
2021-09-30 16:17:32 -0700

    added multiplication

--- app.js
+++ app.js
@@ -7,23 +7,49 @@ const addAnswer = document.getElementById('add-answer');
 console.log(addInput1, addInput2, addButton, addAnswer);
 
 import { sub } from './calculations.js';
-const subNum1 = document.getElementById('sub-number1');
-const subNum2 = document.getElementById('sub-number-2');
+const subInput1 = document.getElementById('sub-number-1');
+const subInput2 = document.getElementById('sub-number-2');
 const subButton = document.getElementById('sub-button');
 const subAnswer = document.getElementById('sub-answer');
 
-console.log(subNum1, subNum2, subButton, subAnswer);
+console.log(subInput1, subInput2, subButton, subAnswer);
+
+import { mult } from './calculations.js';
+const multInput1 = document.getElementById('mult-number-1');
+const multInput2 = document.getElementById('mult-number-2');
+const multButton = document.getElementById('mult-button');
+const multAnswer = document.getElementById('mult-answer');
+
+console.log(multInput1, multInput2, multButton, multAnswer);
+
 
 
 
 addButton.addEventListener('click', ()=>{
-  const value1 = Number(addInput1.value);
-  const value2 = Number(addInput2.value);
-  const result = add(value1, value2);
+    const value1 = Number(addInput1.value);
+    const value2 = Number(addInput2.value);
+    const result = add(value1, value2);
+
+    console.log(result);
+    addAnswer.textContent = result;
+});
+
+subButton.addEventListener('click', ()=>{
+    const value1 = Number(subInput1.value);
+    const value2 = Number(subInput2.value);
+    const result = sub(value1, value2);
+
+    console.log(result);
+    subAnswer.textContent = result;
+});
+
+
+multButton.addEventListener('click', ()=>{
+  const value1 = Number(multInput1.value);
+  const value2 = Number(multInput2.value);
+  const result = mult(value1, value2);
 
   console.log(result);
-  addAnswer.textContent = result;
-})
-
-
+  multAnswer.textContent = result;
+});
 
