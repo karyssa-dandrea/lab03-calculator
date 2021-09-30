d16177399ab1e143bba8eac26faf24016734c3ae
karyssa-dandrea <karyssadandrea@gmail.com>
2021-09-30 15:14:23 -0700

    debugged my addition answer

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-// import functions and grab DOM elements
+import { add } from './calculations.js';
+const addInput1 = document.getElementById('input-1');
+const addInput2 = document.getElementById('input-2');
+const addButton = document.getElementById('add-button');
+const addAnswer = document.getElementById('add-answer');
 
-// initialize global state
+console.log(addInput1, addInput2, addButton, addAnswer);
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+
+addButton.addEventListener('click', ()=>{
+  const value1 = Number(addInput1.value);
+  const value2 = Number(addInput2.value);
+  const result = add(value1, value2);
+
+  console.log(result);
+  addAnswer.textContent = result;
+})
